f1705d5944e73ebce8fe5865f3ab047d8c3bfe02
Manuel Duarte <carlosd99m@gmail.com>
2021-05-14 00:27:49 -0600

    first commit :art:

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+ const App = () => {
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+      <Text>Hola Mundo</Text>
+      <Text>Hola Mundo</Text>
+      <Text>Hola Mundo</Text>
+      <Text>Hola Mundo</Text>
+      <Text>Hola Mundo</Text>
+    </View>
+  )
+}
+
+export default App
