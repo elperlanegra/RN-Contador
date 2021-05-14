8769007cf0dd900b89442e9d1019d8b7f99afec8
Manuel Duarte <carlosd99m@gmail.com>
2021-05-14 00:41:51 -0600

    creando las pantallas

--- App.tsx
+++ App.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import {Text, View} from 'react-native'
+import { HolaMundoScreen } from './src/screens/HolaMundoScreen'
 
- const App = () => {
+
+export const App = () => {
   return (
-    <View style={{ 
-      flex: 1,
-      justifyContent: 'center'
-     }}>
-      <Text style={{ 
-        fontSize: 45,
-        textAlign: 'center'
-      }}>Hola Mundo</Text>
-    </View>
+    <HolaMundoScreen />
   )
 }
 
-export default App
---
+++ src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export const HolaMundoScreen = () => {
+  return (
+    <View style={{ 
+      flex: 1,
+      justifyContent: 'center'
+     }}>
+      <Text style={{ 
+        fontSize: 35,
+        textAlign: 'center'
+      }}>Hola Mundo desde la screen </Text>
+    </View>
+  )
+}
+
