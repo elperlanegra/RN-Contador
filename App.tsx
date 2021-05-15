c6d5de7ad276059c6112d16f881139ac5bff70dd
Manuel Duarte <carlosd99m@gmail.com>
2021-05-14 21:34:30 -0600

    update :fire:

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import {Text, View} from 'react-native'
-import { ContadorScreen } from './src/screens/ContadorScreen'
+import React from 'react';
+import {ContadorScreen} from './src/screens/ContadorScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen'
-
 
 export const App = () => {
   return (
     // <HolaMundoScreen />
     <ContadorScreen />
-  )
-}
-
+  );
+};
--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -2,16 +2,14 @@ import React, {useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 export const ContadorScreen = () => {
-const [contador, setContador] = useState(10);
+
+  const [contador, setContador] = useState(0); // El useSate del contador
 
   return (
-    <View style={styles.containers}>
-      <Text
-        style={styles.texto}>
-        Contador: {contador}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.texto}>Contador: {contador}</Text>
 
-      <TouchableOpacity
+      <TouchableOpacity // Esto es para crear los botones
         style={styles.button}
         onPress={() => setContador(contador + 1)}>
         <Text>Press Here</Text>
@@ -20,16 +18,18 @@ const [contador, setContador] = useState(10);
   );
 };
 
+// Con StyleSheet podemos crear los estilos de los componentes y poder reutilizarlos
 const styles = StyleSheet.create({
-  containers: {
+  container: {
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 10,
   },
   texto: {
     textAlign: 'center',
-    fontSize: 40,
+    fontSize: 35,
     top: -15,
+    padding: 10
   },
   button: {
     alignItems: 'center',
